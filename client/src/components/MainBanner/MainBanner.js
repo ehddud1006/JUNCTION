4fcf8be3b0259c84db2a55ee89db5e4a26aea814
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-12 12:03:50 +0900

    feat: 메인배너 구현

---
+++ client/src/components/MainBanner/MainBanner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MainBanner.css";
+const MainBanner = () => {
+  return (
+    <div className="mainBanner">
+      <div className="title">Main Title On Banner</div>
+      <div className="subTitle">
+        This is how the default size typeface should be presented to the user.
+      </div>
+    </div>
+  );
+};
+
+export default MainBanner;
