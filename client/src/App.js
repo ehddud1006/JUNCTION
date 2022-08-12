ac8ea9d79a728b8a9837557438fe25abf0fe8457
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-12 12:31:50 +0900

    feat: 리액트 라우트 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import MainBanner from "./components/MainBanner/MainBanner";
 import Nav from "./components/Nav/Nav";
 import JustMainPage from "./pages/JustMainPage";
-
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
-      <JustMainPage></JustMainPage>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<JustMainPage></JustMainPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
